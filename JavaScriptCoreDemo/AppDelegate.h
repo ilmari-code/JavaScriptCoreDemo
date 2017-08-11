c3f0710a762dd826e69f6ab47135bcc6709a2505
JiaYuanfangDeveloper <214681658@qq.com>
2017-08-11 16:43:54 +0800

    Initial Commit

---
+++ JavaScriptCoreDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JavaScriptCoreDemo
+//
+//  Created by Mr_Jia on 2017/8/11.
+//  Copyright © 2017年 Mr_Jia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
